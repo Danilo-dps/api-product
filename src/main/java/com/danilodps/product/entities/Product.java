129698f308262731fc9949903a02bd9e402dbd5b
Danilo-dps <danilodpsnilo@outlook.com>
2025-05-17 13:26:05 -0300

    add: modificando a annotation

--- src/main/java/com/danilodps/product/entities/Product.java
+++ src/main/java/com/danilodps/product/entities/Product.java
@@ -23,8 +23,8 @@ public class Product implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(columnDefinition = "UUID")
+    @GeneratedValue
+    @Column(columnDefinition = "UUID", updatable = false, nullable = false)
     private UUID idProduct;
 
     @NotBlank(message = "Nome do produto é obrigatório")
